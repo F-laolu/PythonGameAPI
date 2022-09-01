ba61c6ad573571d9d3f368b1e78394e716cbe354
Olaoluwa Folorunsho <t-olaoluwaf@microsoft.com>
2022-09-01 15:21:31 +0100

    Connected API to cosmos DB with GET, GET by ID and POST method

--- gameScoreDocker/gameScore.py
+++ gameScoreDocker/gameScore.py
@@ -2,19 +2,30 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
+
+from asyncio.windows_events import NULL
 import json
 
+import getpass
+from typing import Collection
+import pymongo
 
+from random import randint
 from flask import Flask, jsonify, request
 
 gameScore = Flask(__name__)
 
-gameResults = [
-    {'id': "0", 'playerName': "Femi", 'score': "170", 'date': "08/02/2021"},
-    {'id': "1", 'playerName': "Ola", 'score': "90", 'date': "22/06/2021"},
-    {'id': "2", 'playerName': "Tola", 'score': "150", 'date': "22/07/2021"},
-    {'id': "3", 'playerName': "Wale", 'score': "160", 'date': "08/12/2021"},
-]
+CONNECTION_STRING = 'mongodb://pythonapi-cosmos:9FSKdBpq9XY3SZg1DmegfgJYWp1ekf3RAp8hQs7Bq3dhs5v5RtDhkloDHed6AXb193Y5bu20dG3RvBTkBK44rg==@pythonapi-cosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@pythonapi-cosmos@' 
+DB_NAME = "pythongame"
+UNSHARDED_COLLECTION_NAME = "gameResult"
+SAMPLE_FIELD_NAME = "sample_field"
+
+#gameResults = [
+    #{'id': "0", 'playerName': "Femi", 'score': "170", 'date': "08/02/2021"},
+    #{'id': "1", 'playerName': "Ola", 'score': "90", 'date': "22/06/2021"},
+    #{'id': "2", 'playerName': "Tola", 'score': "150", 'date': "22/07/2021"},
+    #{'id': "3", 'playerName': "Wale", 'score': "160", 'date': "08/12/2021"},
+#]
 
 
 @gameScore.route('/')
@@ -23,11 +34,26 @@ def index():
 
 @gameScore.route("/gameResults", methods=['GET'])
 def get():
+    game = collection.find()
+    gameResults = []
+    for doc in game:
+        print (doc)
+        gameResult = {
+           'id': doc['id'], 'playerName': doc['playerName'], 'score': doc['score'], 'date': doc['date'] 
+        }
+        gameResults.append(gameResult)
     return jsonify({'gameresult': gameResults})
-
 
 @gameScore.route("/gameResults/<int:id>", methods=['GET'])
 def get_index(id):
+    games = collection.find()
+    gameResults = []
+    for doc in games:
+        print (doc)
+        gameResult = {
+           'id': doc['id'], 'playerName': doc['playerName'], 'score': doc['score'], 'date': doc['date'] 
+        }
+        gameResults.append(gameResult)
     return jsonify({'gameresult': gameResults[id]})
 
 
@@ -42,10 +68,16 @@ def create():
         'score': req_data['score'],
         'date': req_data['date']
     }
-    gameResults.append(new_results)
-    return jsonify(new_results)
-    #return jsonify({'gameresult': gameResults[new_results]})
-
-
+    document_id = collection.insert_one(new_results).inserted_id
+    return str(document_id)
+   
 if __name__ == "__main__":
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    try:
+        client.server_info() # validate connection string
+    except pymongo.errors.ServerSelectionTimeoutError:
+        raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
+
+    collection = client[DB_NAME][UNSHARDED_COLLECTION_NAME]
+    print (collection)
     gameScore.run(host='0.0.0.0', port = 5000, debug=True)
