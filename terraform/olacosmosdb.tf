22f9fca1d6c2dfca06a92218d85741cf5c798ed7
F-laolu <79999560+F-laolu@users.noreply.github.com>
2022-12-02 10:06:52 +0000

    Update olacosmosdb.tf

--- terraform/olacosmosdb.tf
+++ terraform/olacosmosdb.tf
@@ -28,7 +28,7 @@ resource "azurerm_cosmosdb_sql_container" "dbcon" {
   resource_group_name   = azurerm_resource_group.rg.name
   account_name          = azurerm_cosmosdb_account.dbacc.name
   database_name         = azurerm_cosmosdb_sql_database.db.name
-  partition_key_path    = "/definition/id"
+  partition_key_path    = "/id"
   partition_key_version = 1
   throughput            = 400
 
